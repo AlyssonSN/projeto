90756bc9619a83a8d88cc2698f6221ad4fb4475f
AlyssonSN <64441530+AlyssonSN@users.noreply.github.com>
2022-05-29 00:58:35 -0400

    Add files via upload

--- atividade/java.js
+++ atividade/java.js
@@ -1,5 +1,34 @@
 function caucula() {
     let peso = document.getElementById("peso").value;
-    peso = parseFloat (peso);
-    let agua = peso * (35);
+    console.log(peso);
+    peso = Number(peso);
+    let agua = peso * 35;
+    console.log(agua)
+
+    if (idade <= 17) {
+        peso = Number(peso);
+        let agua = peso * 40;
+        console.log(agua)
+    }
+
+    else if (idade >= 18 && idade <= 55) {
+        peso = Number(peso);
+        let agua = peso * 35;
+        console.log(agua)
+    }
+
+    else if (idade >= 56 && idade <= 65) {
+        peso = Number(peso);
+        let agua = peso * 35;
+        console.log(agua)
+    }
+
+    else if (idade >= 66) {
+        peso = Number(peso);
+        let agua = peso * 25;
+        console.log(agua)
+    }
+
+    const resultado = document.getElementById("resultado")
+    resultado.innerHTML = `Você deve tomar ${agua} ml diariamente`;
 }
